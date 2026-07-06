fd0fc5a0a864845a3ff024d64a631b4703e44cf6
tarikulislam <tarikulislam4145@gmail.com>
2026-07-06 22:58:42 +0530

    add stun

--- playground/p2p/script.js
+++ playground/p2p/script.js
@@ -2,3 +2,21 @@
 
 const localVideo = document.getElementById("LocalVideo");
 const remoteVideo = document.getElementById("RemoteVideo");
+
+ 
+let localStream;
+let peerConnection;
+
+
+const servers = {
+    iceServers: [
+        {
+            urls: ["stun:stun.l.google.com:19302",
+                "stun:stun1.l.google.com:19302",
+                "stun:stun2.l.google.com:19302",
+                "stun:stun3.l.google.com:19302",
+                "stun:stun4.l.google.com:19302"]
+        }, 
+        
+    ] 
+}
