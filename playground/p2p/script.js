02037d46c703cabc234a217b95cb5f66dd3c3bf6
tarikulislam <tarikulislam4145@gmail.com>
2026-07-08 23:25:30 +0530

    working

--- playground/p2p/script.js
+++ playground/p2p/script.js
@@ -18,10 +18,7 @@ const servers = {
   iceServers: [
     {
       urls: [
-        "stun:stun.l.google.com:19302",
-        "stun:stun2.l.google.com:19302"
-        
-      ]
+        "stun:stun.l.google.com:19302"]
     },
     {
       urls: [
