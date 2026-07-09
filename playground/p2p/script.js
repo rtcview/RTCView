8503174a708d23b5166e110262730d9d4a856a84
tarikulislam <tarikulislam4145@gmail.com>
2026-07-09 19:13:47 +0530

    media access

--- playground/p2p/script.js
+++ playground/p2p/script.js
@@ -2,5 +2,41 @@
 
 
 const RemoteVideo = document.getElementById('RemoteVideo');
-const LocalVideo = document.getElementById('LocalVideo');
+const LocalVideo = document.getElementById('LocalVideo'); 
 
+let localStream;
+let peerConnection;
+let roomID;
+
+const servers = {
+
+    iceServers:[ 
+{
+urls: [ 
+"stun:stun.l.google.com:19302",
+ "stun:stun2.l.google.com:19302"
+]
+
+},
+{ urls: [
+  "turn:openrelay.metered.ca:80",
+  "turn:openrelay.metered.ca:443",
+  "turn:openrelay.metered.ca:443?transport=tcp"
+],
+ username: "openrelayproject",
+ credential: "openrelayproject"
+ }
+
+]
+
+};
+
+// init media access
+async function initMedia() {
+ localStream = await navigator.mediaDevices.getUserMedia({
+    video: true,
+    audio: true,
+ });
+
+    
+}
